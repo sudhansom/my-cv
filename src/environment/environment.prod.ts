4d79c948df53147727a0711d6baad78ff8aef0e8
bhuwan <bks@sitemule.com>
2023-06-05 23:37:20 +0200

    update production environment

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -15,7 +15,7 @@ export const environment = {
         details: 'In my current job, I am using SCSS with BEM technology in angular.'
       },
       {
-        tech: 'javascrip',
+        tech: 'javascript',
         details: 'I have learned few programming languages such as Java, Python, C, C++, I am coding with Javascript in my current job and its fun.'
       },
       {
@@ -30,6 +30,118 @@ export const environment = {
         tech: 'Node.js',
         details: 'I have good knowledge in vanilla Node.js. I have developed few backend APIs using express.'
       },
+      {
+        tech: 'React js',
+        details: 'Created few projects using React js and Express.',
+      },
     ],
+    myself:
+    {
+      name: 'Bhuwan Krishna Som Poudel',
+      image: '../assets/images/my-image.png',
+      phone: '004581916893',
+      email: 'bkspoudel@gmail.com',
+      linkedIn: 'https://www.linkedin.com/feed/',
+      gitHub: 'https://github.com/sudhansom?tab=repositories',
+      address: 'Copenhagen, Denmark',
+    },
+    experience : [
+      {
+        type: 'Education',
+        all: [
+          {
+            name: 'Norwegian University of Science and Technology',
+            position: 'Masters Degree in Information Systems',
+            address: 'Trondheim, Norway',
+            date: '08/2011 - 10/2014',
+            features: [
+              'Web Intelligence',
+              'Software Security',
+              'Model Driven Development of Information Systems',
+              'Customer Driven Project',
+              'Software Quality and Process Improvement',
+            ],
+            note: 'Learned Norwegian language for a year and completed 30 credits',
+          },
+          {
+            name: 'Advanced Engineering College',
+            position: 'Bachelors in Computer Enginnering',
+            address: 'Kathmandu, Nepal',
+            date: '08/2001 - 10/2005',
+            features: [
+              'Computer programming with C, C++',
+              'Artificial Intelligence',
+              'Software Security',
+              'Digital Signal Processing',
+              'Microprocessor - Machine Learning',
+            ],
+            note: 'Learned Norwegian language for a year and completed 30 credits',
+          },
+        ]
+      },
+      {
+        type: 'Work Experience',
+        all: [
+          {
+            name: 'System & Metode A/S',
+            position: 'Frontend Developer',
+            address: 'Hørsholm, Denmark',
+            date: '06/2022 - running',
+            features: [
+              'Pixel Perfect design',
+              'Angular Framework',
+              'Javascript',
+              'Scss',
+              'Html',
+            ],
+            note: 'Learning new things everyday while working in real projects.',
+          },
+          {
+            name: 'Integrify Academy International',
+            position: 'Fullstack Developer',
+            address: 'Helsinki, Finland (Remote)',
+            date: '08/2021 - 07/2022',
+            features: [
+              'Todo-app using Bootstrap, Sass, Html, React/js',
+              'Data Analysis - MongoDB, Node.js React',
+              'Simple Web-app using RestAPI',
+              'Customer Driven Project',
+              'Learned React, Express, Redux, Html, Css , TypeScript',
+            ],
+            note: 'Learned to work in team.',
+          },
+        ]
+      },
+      {
+        type: 'Leadership Experience',
+        all: [
+          {
+            name: 'Cofoco Airport Solution',
+            position: 'Packing Assistant (Team Leader)',
+            address: 'Kaastrup, Denmark',
+            date: '06/2015 - 01/2020',
+            features: [
+              'Led a team of 10 - 12 members to consistently pack food for more than 2000 customers every day.',
+              'Contributed towards the firm as a Logistic assistant for one year before promoted to a team leader.',
+              'Developed leadership skills to work in a team.',
+              'During this period to stay in touch in programming I used to solve online coding challenges in HackerRank, CodeWars, and AlgoExpert (especially in Java and Python).',
+            ],
+            note: 'Learned Danish while working in team.',
+          },
+          {
+            name: 'Sagarmatha Television',
+            position: 'Engineer / Technician',
+            address: 'Kathmandu, Nepal',
+            date: '09/2008 - 01/2011',
+            features: [
+              'Developed instructions to the news reader and all the technicians improving my interpersonal skills.',
+              'Managed a team of more than 15 members to work on several projects that involved a large community.',
+            ],
+            note: 'Learned Norwegian language for a year and completed 30 credits',
+          },
+        ]
+      }
+    ],
+    languages: [ 'Nepali (Mother Tongue)', 'English', 'Danish(B1)', 'Hindi', 'Norwegian(B1)']
   }
 }
